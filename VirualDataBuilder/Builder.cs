1e57ede1cf84a49726bbe00a1fd5e4ab30498517
wangjian <wangjian@vcyber.com>
2015-05-22 22:17:21 +0800

    draft

---
+++ VirualDataBuilder/Builder.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualDataBuilder
+{
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.Remoting.Messaging;
+
+    using log4net;
+
+    public class HeatReader
+    {
+        private int iUploadTime = 3;
+        private int iHeat = 13;
+        private int iMeterId = 24;
+
+        List<string[]> heater = new List<string[]>();
+        public Dictionary<DateTime, int> Result = new Dictionary<DateTime, int>();
+
+        private ILog log = LogManager.GetLogger(typeof(HeatReader));
+        public void ReadHeater(string fileName)
+        {
+            string[] contents = File.ReadAllLines(fileName, Encoding.UTF8);
+            for (int i = 1; i < contents.Length; i++)
+            {
+                var line = contents[i].Trim();
+                if(string.IsNullOrEmpty(line))continue;
+                string[] temp = line.Split(new []{','});
+                //heater.Add(temp);
+                DateTime time;
+                int heat;
+                if (DateTime.TryParse(temp[iUploadTime], out time) && int.TryParse(temp[this.iHeat], out heat))
+                {
+                    if (this.Result.ContainsKey(time))
+                    {
+                        this.Result[time] += heat;
+                    }
+                    else
+                    {
+                        this.Result.Add(time, heat);
+                    }
+                }
+                else
+                {
+                    log.Error(string.Format("Parse failed: [{0}], [{1}]",temp[iUploadTime], temp[iHeat]));
+                }
+            }
+        }
+
+
+       
+
+
+
+    }
+
+    public class Builder
+    {
+        private int iRunTime = 10;
+
+        private int iTotalTime = 11;
+
+        private int iRoomTemperature = 14;
+
+        private int iSetTemperature = 15;
+
+        private int iShangQuDuanKaiQiShiJian = 24;
+
+        private int iShangQuDuanZongShiJian = 25;
+
+        private int iShangQuDuanKaiDu = 26;
+        private int iShangQuDuanPingJunWenDu = 27;
+
+        private int iTotalQuantityHeat = 28;
+
+        private int iRoomSize = 8;
+
+        private int roomSizeTotal = 0;
+
+        private int iQuDuanFenTanReLiang = 32;
+
+        List<string[]> content= new List<string[]>();
+        private ILog log = LogManager.GetLogger(typeof(Builder));
+       
+
+        public string Build(string controllerFile, string heaterFile)
+        {
+            //read controller file
+            string[] tempContent = File.ReadAllLines(controllerFile,Encoding.UTF8);
+            int maxId = 0;
+           for(int i=1;i< tempContent.Length;i++)
+           {
+               if (string.IsNullOrEmpty(tempContent[i])) continue;
+               string[] temp = tempContent[i].Split(new []{','});
+               
+               maxId = int.Parse(temp[0]);
+               content.Add(temp);
+               int roomSize;
+               if (int.TryParse(temp[iRoomSize], out roomSize))
+               {
+                   roomSizeTotal += roomSize;
+               }
+               else
+               {
+                   log.Error("Parse room size failed: ["+temp[iRoomSize]+"]");
+               }
+            }
+
+            //begin write
+            string newFileName = Path.Combine(
+                Path.GetDirectoryName(controllerFile),
+                Path.GetFileNameWithoutExtension(controllerFile) + "_" + DateTime.Now.ToString("yyyyMMddHHmm") + ".csv");
+            if(File.Exists(newFileName))File.Delete(newFileName);
+            File.Copy(controllerFile,newFileName);
+            using (StreamWriter sw = new StreamWriter(newFileName, true, Encoding.UTF8))
+            {
+                sw.WriteLine();
+                HeatReader hr = new HeatReader();
+                hr.ReadHeater(heaterFile);
+                int runTime = int.Parse(content[0][iRunTime]);
+                int totalTime = int.Parse(content[0][this.iTotalTime]);
+                int deltaHeat = 0;
+                for (int i = 1; i < hr.Result.Count; i++)
+                {
+                    var heat1 = hr.Result.Keys.ElementAt(i-1);
+                    var heat2 = hr.Result.Keys.ElementAt(i);
+                    int heatDiff = hr.Result[heat2] - hr.Result[heat1]+deltaHeat;
+                    int heatTotalThisTime = 0;
+                    TimeSpan timeDiff = heat2 - heat1;
+
+                    for (int j = 0; j < content.Count; j++)
+                    {
+                        string[] newData = (string[])content[j].Clone();
+                        runTime += timeDiff.Hours;
+                        totalTime += timeDiff.Hours;
+                        //newData[0] = (++maxId).ToString();
+                        newData[1] = heat2.ToString("yyyy/M/d H:mm");
+                        //newData[iRunTime] = runTime.ToString("0");
+                        //newData[this.iTotalTime] = totalTime.ToString("0");
+                        //newData[iRoomTemperature] = "0";
+                        //newData[iSetTemperature] = "0";
+                        //newData[iShangQuDuanKaiQiShiJian] = "2";
+                        //newData[iShangQuDuanZongShiJian] = "2";
+                        //newData[iShangQuDuanKaiDu] = "1000";
+                        //newData[iShangQuDuanPingJunWenDu] = "1000";
+                        double distribution = heatDiff * (int.Parse(newData[iRoomSize]) * 1.0) / roomSizeTotal;
+                        int changedHeat = (int)Math.Round(distribution, 0);
+                        heatTotalThisTime += changedHeat;
+                        newData[iTotalQuantityHeat] = (int.Parse(content[j][iTotalQuantityHeat]) + changedHeat).ToString();
+                        newData[iQuDuanFenTanReLiang] = Math.Round(distribution * 1000,0).ToString("0");
+                        sw.WriteLine(string.Join(",",newData));
+                        content[j] = (string[])newData.Clone();
+                    }
+                    deltaHeat = heatDiff - heatTotalThisTime;
+                }
+            }
+            return newFileName;
+        }
+    }
+}
---
+++ VirualDataBuilder/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VirtualDataBuilder
+{
+    using System.Diagnostics;
+
+    public partial class Form1 : Form
+    {
+        private string filePath = string.Empty;
+
+        private string[] heatFiles;
+        private string[] controllerFiles;
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            this.textBox1.AllowDrop = true;
+            textBox1.DragEnter += panel1_DragEnter;
+            textBox1.DragDrop += panel1_DragDrop;
+
+            this.textBox2.AllowDrop = true;
+            textBox2.DragEnter += panel2_DragEnter;
+            textBox2.DragDrop += panel2_DragDrop;
+        }
+
+       
+
+        void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            this.heatFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var item in this.heatFiles)
+            {
+                textBox1.Text += item + Environment.NewLine;
+            }
+        }
+
+        void panel1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+        
+        private void panel2_DragDrop(object sender, DragEventArgs e)
+        {
+            textBox2.Text = string.Empty;
+            this.controllerFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var item in this.controllerFiles)
+            {
+                textBox2.Text += item + Environment.NewLine;
+            }
+        }
+        private void panel2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string heatFile = @"E:\temp\20150521\heater.csv";
+            //string controllerFile = @"E:\temp\20150521\controller.csv";
+            Builder builder = new Builder();
+            txtMsg.Text = "处理中...";
+            string resultFile = builder.Build(controllerFiles[0], heatFiles[0]);
+            txtMsg.Text = "完成. ["+resultFile+"]";
+        }
+
+       
+    }
+}
